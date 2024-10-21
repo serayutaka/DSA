37debce0cfdad55ce2080b41c0f5b36336cd578d
Copter <121752252+serayutaka@users.noreply.github.com>
2024-10-21 15:26:24 +0700

    Insertion

--- sorting/Insertion/src/Main.java
+++ sorting/Insertion/src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP
+// <b>Insertion Sort</b>
+// <li>Time Complexity: <b>O(n^2)</b> (Average), <b>O(n)</b> (Best), <b>O(n^2)</b> (Worst)</li>
+// <li>Space Complexity: <b>O(1)</b></li>
+// <li>Characteristics</li>
+// <ul>
+// <li>Efficient for small or nearly sorted data sets but less efficient for large or unsorted dataset</li>
+// <li>It's stable sorting algorithm</li>
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = {4, 2, 3, 1};
+        InsertionSort(arr);
+        for (int elem : arr) {
+            System.out.printf("%d ", elem);
+        }
+        System.out.println();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void InsertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
     }
 }
