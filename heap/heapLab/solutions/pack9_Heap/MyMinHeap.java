4030183379aa39a5775bf0282bbfed4de7e66d24
Copter <121752252+serayutaka@users.noreply.github.com>
2024-10-25 01:32:27 +0700

    min heap lab

--- heap/heapLab/solutions/pack9_Heap/MyMinHeap.java
+++ heap/heapLab/solutions/pack9_Heap/MyMinHeap.java
@@ -6,10 +6,50 @@ public class MyMinHeap {
     ArrayList<Integer> v = new ArrayList<>();
 
     public void insert(int value) {
-        
-    }
-    public int remove() {
-        return 0;
+        v.add(value);
+        int curIndex = v.size() - 1;
+
+        while (curIndex != 0) {
+            int parentIndex = (curIndex - 1) / 2;
+
+            if (v.get(parentIndex) > v.get(curIndex)) {
+                swap(curIndex, parentIndex);
+            }
+            curIndex = parentIndex;
+        }
     }
 
+    public int remove() {
+        int lastIndex = v.size() - 1;
+        int temp = v.getFirst();
+        v.set(0, v.get(lastIndex));
+        v.set(lastIndex, temp);
+        v.remove(lastIndex);
+        heapify(0);
+        return temp;
+    }
+    private void heapify(int index) {
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        if (leftChildIndex < v.size() && v.get(leftChildIndex) < v.get(index)) {
+            swap(leftChildIndex, index);
+            heapify(leftChildIndex);
+        }
+        if (rightChildIndex < v.size() && v.get(rightChildIndex) < v.get(index)) {
+            swap(rightChildIndex, index);
+            heapify(rightChildIndex);
+        }
+    }
+
+    public void swap(int i, int j) {
+        int temp = v.get(i);
+        v.set(i, v.get(j));
+        v.set(j, temp);
+    }
+
+    @Override
+    public String toString() {
+        return v.toString();
+    }
 }
